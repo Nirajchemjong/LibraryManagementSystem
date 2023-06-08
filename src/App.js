b2e553110dca17d741c3ec12d24b8d7a7ac26fea
Nirajchemjong <nirajlimbu7@gmail.com>
2023-06-08 14:09:47 +1000

    sign up and working with firebase

--- src/App.js
+++ src/App.js
@@ -3,30 +3,34 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/sign-up-sign-in/Login";
 import Signup from "./pages/sign-up-sign-in/Signup";
 import Home from "./pages/Home/Home";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path='/login'
-        element={<Login />}
-      ></Route>
-      <Route
-        path='/signup'
-        element={<Signup />}
-      ></Route>
+    <>
+      <Routes>
+        <Route
+          path='/login'
+          element={<Login />}
+        ></Route>
+        <Route
+          path='/signup'
+          element={<Signup />}
+        ></Route>
 
-      <Route
-        path='/'
-        element={<Home />}
-      ></Route>
+        <Route
+          path='/'
+          element={<Home />}
+        ></Route>
 
-      {/* //{" "}
+        {/* //{" "}
       <div className=''>
         // <Button variant='primary'>Primary</Button>
         //{" "}
       </div> */}
-    </Routes>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
--- src/pages/sign-up-sign-in/Signup.js
+++ src/pages/sign-up-sign-in/Signup.js
@@ -4,16 +4,54 @@ import { DefaultLayout } from "../../components/layout/DefaultLayout";
 import Form from "react-bootstrap/Form";
 import { CustomInput } from "../../components/custom-input/CustomInput";
 import { Button } from "react-bootstrap";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../config/firebase-config";
+import { toast } from "react-toastify";
+
 const Signup = () => {
   const [form, setForm] = useState({});
 
   const handleOnChanged = (e) => {
     const { name, value } = e.target;
-    console.log(name, value);
+    // console.log(name, value);
     setForm({
       ...form,
       [name]: value,
     });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const { password, confirmPassword } = form;
+    if (password !== confirmPassword) {
+      return toast.error("password do not match");
+    }
+    // console.log(e);
+
+    try {
+      //use firebase to create user and store user
+
+      const authSnapPromise = createUserWithEmailAndPassword(
+        auth,
+        form.email,
+        password
+      );
+      toast.promise(authSnapPromise, {
+        pending: "please wait..",
+      });
+      const authSnap = await authSnapPromise;
+      console.log(authSnap);
+      if (authSnap?.user.uid) {
+        toast.success("New user has been created");
+      }
+    } catch (error) {
+      console.log(error);
+      let msg = error.message;
+      if (msg.includes("auth/email-already-in-use")) {
+        msg = " There is already user exist with this email";
+      }
+      toast.error(msg);
+    }
   };
   const inputs = [
     {
@@ -52,21 +90,22 @@ const Signup = () => {
       type: "password",
       placeholder: "**********",
       required: true,
+      minLength: 6,
     },
     {
       lable: "Confirm Password",
-      name: "password",
+      name: "confirmPassword",
       type: "password",
       placeholder: "***********",
       required: true,
     },
   ];
-  console.log(form);
+  //   console.log(form);
 
   return (
     <DefaultLayout>
       <div className='admin-form border p-3'>
-        <Form>
+        <Form onSubmit={handleOnSubmit}>
           <h1>Admin Registration </h1>
 
           {inputs.map((item) => (

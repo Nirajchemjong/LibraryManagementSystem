e0299379d9aa3eeacca889a68054b1c78c33692d
Nirajchemjong <nirajlimbu7@gmail.com>
2023-06-20 18:46:45 +1000

    learning chart

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import { DefaultLayout } from "../../components/layout/DefaultLayout";
 import { UserLayout } from "../../components/layout/UserLayout";
+import { PieChart } from "../Chart/PieChart";
 
 export const Dashboard = () => {
   return (
@@ -8,6 +9,9 @@ export const Dashboard = () => {
       <UserLayout>
         <h3 className='text-center mt-3'>Dashboard</h3>
         <hr />
+        <div style={{ width: "40rem", height: "20rem" }}>
+          <PieChart />
+        </div>
       </UserLayout>
     </div>
   );
